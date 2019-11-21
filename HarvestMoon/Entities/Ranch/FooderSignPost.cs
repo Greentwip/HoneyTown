729bd2f8047afc560540bafca5baf267a52f8811
Victor Lopez <lvictor_9@hotmail.com>
2019-11-21 05:25:22 -0700

    Add signposts

---
+++ HarvestMoon/Entities/Ranch/FooderSignPost.cs
@@ -0,0 +1,26 @@
+﻿using HarvestMoon.Entities.General;
+using Microsoft.Xna.Framework;
+using MonoGame.Extended;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarvestMoon.Entities.Ranch
+{
+    public class FooderSignPost : NPC
+    {
+        public FooderSignPost(Vector2 initialPosition, Size2 size, bool displaysMessage, string message)
+            : base(initialPosition, size, displaysMessage, message)
+        {
+
+        }
+
+        public override string GetMessage(Item item)
+        {
+            var replacedPieces = Message.Replace("number", HarvestMoon.Instance.FeedPieces.ToString());
+            return replacedPieces;
+        }
+    }
+}
---
+++ HarvestMoon/Entities/Ranch/WoodSignPost.cs
@@ -0,0 +1,26 @@
+﻿using HarvestMoon.Entities.General;
+using Microsoft.Xna.Framework;
+using MonoGame.Extended;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarvestMoon.Entities.Ranch
+{
+    public class WoodSignPost : NPC
+    {
+        public WoodSignPost(Vector2 initialPosition, Size2 size, bool displaysMessage, string message)
+            : base(initialPosition, size, displaysMessage, message)
+        {
+
+        }
+
+        public override string GetMessage(Item item)
+        {
+            var replacedPieces = Message.Replace("number", HarvestMoon.Instance.Planks.ToString());
+            return replacedPieces;
+        }
+    }
+}
